cbc25355c8dd758417905331d6d54c0d34470e7e
Wenxuan Zhao <viz@linux.com>
2018-10-09 04:44:22 -0700

    Initial commit, add first release

---
+++ Hook/InjectionEntryPoint.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TaiwuModLoader
+{
+    /// <summary>
+    /// The EasyHook injection entrypoint.
+    /// </summary>
+    public class InjectionEntryPoint : EasyHook.IEntryPoint
+    {
+        /// <summary>
+        /// Reference to the server interface within TaiwuFragmentsHook.
+        /// </summary>
+        ServerInterface _server = null;
+
+        /// <summary>
+        /// Message queue of all files accessed.
+        /// </summary>
+        Queue<string> _messageQueue = new Queue<string>();
+
+        /// <summary>
+        /// Constructor for the EasyHook entrypoint.
+        /// </summary>
+        /// <param name="context">The RemoteHooking context</param>
+        /// <param name="channelName">The name of the IPC channel</param>
+        public InjectionEntryPoint(EasyHook.RemoteHooking.IContext context, string channelName)
+        {
+            // Connect to server object using provided channel name
+            _server = EasyHook.RemoteHooking.IpcConnectClient<ServerInterface>(channelName);
+
+            // If Ping fails then the Run method will be not be called
+            _server.Ping();
+        }
+
+        /// <summary>
+        /// Logic of the entrypoint.
+        /// </summary>
+        /// <param name="context">The RemoteHooking context</param>
+        /// <param name="channelName">The name of the IPC channel</param>
+        public void Run(EasyHook.RemoteHooking.IContext context, string channelName)
+        {
+            EasyHook.LocalHook monoOpenImageHook = null;
+            var filename = @"Mono\EmbedRuntime\mono.dll";
+            LoadLibraryW(filename);
+
+            // Install the hook
+            IntPtr pTargetProc = EasyHook.LocalHook.GetProcAddress(filename, "mono_image_open_from_data_with_name");
+            monoOpenImageHook = EasyHook.LocalHook.Create(pTargetProc, new MonoOpenImage_Delegate(MonoOpenImage_Hook), this);
+            monoOpenImageHook.ThreadACL.SetExclusiveACL(new int[] { 0 });
+            
+            // Wake up the process
+            EasyHook.RemoteHooking.WakeUpProcess();
+
+            while (true)
+            {
+                Thread.Sleep(500);
+
+                string[] queued = null;
+
+                lock (_messageQueue)
+                {
+                    queued = _messageQueue.ToArray();
+                    _messageQueue.Clear();
+                }
+
+                // Send newly received message back to Loader.exe
+                if (queued != null && queued.Length > 0)
+                {
+                    _server.OutputMessages(queued);
+                }
+                else
+                {
+                    _server.Ping();
+                }
+            }
+        }
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern IntPtr LoadLibraryW(string lpFileName);
+
+        #region ImageOpen Hook
+        delegate IntPtr MonoOpenImage_Delegate(IntPtr data, uint dataLen, bool needCopy, IntPtr status, bool refOnly, string name);
+
+        [DllImport("mono.dll", EntryPoint = "mono_image_open_from_data_with_name", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr MonoOpenImage(IntPtr data, uint dataLen, bool needCopy, IntPtr status, bool refOnly, string name);
+
+        IntPtr MonoOpenImage_Hook(IntPtr data, uint dataLen, bool needCopy, IntPtr status, bool refOnly, string name)
+        {
+            const string originalFilename = @"Assembly-CSharp.dll";
+            const string dumpedFilename = @"Assembly-CSharp-Patched.dll";
+            
+            if (name.EndsWith(originalFilename))
+            {
+                var originalPath = name;
+                var patchedPath = originalPath.Replace(originalFilename, dumpedFilename);
+                _server.OutputMessage(string.Format("Detour {0} to: {1}", originalFilename, dumpedFilename));
+                
+                byte[] assemblyData = File.ReadAllBytes(patchedPath);
+                data = Marshal.AllocHGlobal(assemblyData.Length);
+                dataLen = (uint)assemblyData.Length;
+                name = patchedPath;
+
+                Marshal.Copy(assemblyData, 0, data, (int)dataLen);
+            }
+
+            return MonoOpenImage(data, dataLen, needCopy, status, refOnly, name);
+        }
+        #endregion
+    }
+}
---
+++ Loader/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using UnityModManagerNet;
+
+namespace TaiwuModLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string originalFilename = @"Assembly-CSharp.dll";
+            const string patchedFilename = @"Assembly-CSharp-Patched.dll";
+
+            // find the assembly
+            string originalPath;
+            string[] matches = Directory.GetFiles(
+                Directory.GetCurrentDirectory(),
+                originalFilename,
+                SearchOption.AllDirectories
+            );
+            if (matches.Length != 1)
+            {
+                Console.Error.WriteLine(string.Format("Error: Failed to find \"{0}\"", originalFilename));
+                return;
+            }
+            originalPath = matches[0];
+            Console.WriteLine(string.Format("Found assembly: {0}", originalPath));
+
+            // load the assembly image
+            byte[] assemblyData = AssemblyLoader.LoadImage(@"Mono\EmbedRuntime\mono.dll", originalPath);
+
+            // patch the assembly image
+            ModuleDefMD assembly = ModuleDefMD.Load(assemblyData);
+
+            TypeDef targetClass = assembly.Types.FirstOrDefault((TypeDef x) => x.FullName == "DateFile");
+            MethodDef targetMethod = targetClass.Methods.FirstOrDefault((MethodDef x) => x.Name == "Awake");
+
+            Type modManagerType = typeof(UnityModManager);
+            ModuleDefMD moduleDefMD = ModuleDefMD.Load(modManagerType.Module);
+            TypeDef modManager = moduleDefMD.Types.First((TypeDef x) => x.Name == modManagerType.Name);
+            modManager.Fields.First((FieldDef x) => x.Name == "modsDirname").Constant.Value = "Mods";
+            modManager.Fields.First((FieldDef x) => x.Name == "infoFilename").Constant.Value = "Info.json";
+            moduleDefMD.Types.Remove(modManager);
+            assembly.Types.Add(modManager);
+            Instruction instr = OpCodes.Call.ToInstruction(modManager.Methods.First((MethodDef x) => x.Name == "Start"));
+            targetMethod.Body.Instructions.Insert(targetMethod.Body.Instructions.Count - 1, instr);
+
+            // write the patched assembly image
+            string patchedPath = originalPath.Replace(originalFilename, patchedFilename);
+            assembly.Write(patchedPath);
+            Console.WriteLine(string.Format("Assembly patched: {0}", patchedPath));
+
+            // copy library
+            string libraryPath = originalPath.Replace(originalFilename, "");
+            File.Copy(Path.Combine(Directory. GetCurrentDirectory(), "0Harmony12.dll"), Path.Combine(libraryPath, "0Harmony12.dll"), true);
+
+            int targetPID = 0;
+            const string targetExe = "The Scroll Of Taiwu Alpha V1.0.exe";
+
+            string channelName = null;
+
+            EasyHook.RemoteHooking
+                .IpcCreateServer<ServerInterface>(
+                ref channelName,
+                System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+            
+            string injectionLibrary = Path.Combine(Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().Location),
+                "Hook.dll");
+
+            try
+            {
+                EasyHook.RemoteHooking.CreateAndInject(
+                    targetExe,
+                    "",
+                    0,
+                    EasyHook.InjectionOptions.DoNotRequireStrongName,
+                    injectionLibrary,
+                    injectionLibrary,
+                    out targetPID,
+                    channelName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            var p = Process.GetProcessById(targetPID);
+            p.WaitForExit();
+
+            Console.WriteLine("Game process exited, press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
