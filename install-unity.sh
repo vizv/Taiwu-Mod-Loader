52c9371ab9fe7d916b74568d796b3e8a25a62e75
Wenxuan Zhao <viz@linux.com>
2018-10-12 13:10:29 -0700

    Improve build caching, update .gitignore

--- install-unity.sh
+++ install-unity.sh
@@ -10,4 +10,10 @@ UNITY_VERSION="$(cat Unity.version)"
 
 curl -o /tmp/UnitySetup64.exe "https://download.unity3d.com/download_unity/38bd7dec5000/Windows64EditorInstaller/UnitySetup64-${UNITY_VERSION}f1.exe"
 7z -oUnity x /tmp/UnitySetup64.exe
+
+mkdir -p Unity/lib
+cp Unity/Editor/Data/Managed/UnityEngine/* Unity/lib
+cp Unity/Editor/Data/UnityExtensions/Unity/GUISystem/Standalone/* Unity/lib
+rm -rf Unity/{Editor,\$PLUGINSDIR}
+
 echo "${UNITY_VERSION}" > Unity/version
